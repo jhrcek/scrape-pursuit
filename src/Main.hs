e6f9cd3b760f84df7ee8ae6ffb01664479fdce68
Jan Hrcek <jhrcek@redhat.com>
2019-01-28 10:16:41 +0100

    Use local pursuit server

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,23 @@
 {-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wall #-}
 module Main where
 import           Conduit        (concatC, liftIO, mapMC, mapM_C, runConduit,
                                  yieldMany, (.|))
-import           Data.Maybe     (fromMaybe)
+import           Data.Maybe     (catMaybes, fromMaybe)
 import           Data.Text      (Text, unpack)
 import qualified Data.Text      as Text
+
 import qualified Data.Text.IO   as Text
-import           Test.WebDriver (Selector (ByCSS), WD, WDConfig, chrome,
-                                 defaultConfig, finallyClose, findElems,
+import           Test.WebDriver (Selector (ByCSS, ByClass), WD, WDConfig, attr,
+                                 chrome, defaultConfig, finallyClose, findElems,
                                  getText, openPage, runSession, useBrowser)
 
 main :: IO ()
-main = do
-    urls <- liftIO $ Text.lines <$> Text.readFile "x"
-    runSession chromeCaps . finallyClose . runConduit $
-        yieldMany urls
+main = runSession chromeCaps . finallyClose $ do
+    pkgLinks <- getAllPackageLinks
+    runConduit $
+        yieldMany pkgLinks
         .| mapMC getDependencyEdges
         .| concatC
         .| mapM_C (liftIO . Text.putStrLn)
@@ -23,13 +25,15 @@ main = do
 getDependencyEdges :: Text -> WD [Text]
 getDependencyEdges url = do
     openPage (unpack url)
-    deps <- traverse getText =<< findElems (ByCSS ".deplink__link")
+    deps <- traverse getText =<< findElems (ByClass "deplink__link")
     let pkgName = last $ Text.splitOn "/" url
     return $ mkEdges pkgName deps
 
 mkEdges :: Text -> [Text] -> [Text]
 mkEdges pkgName =
-    fmap (\dep -> "\"" <> dropPurescript pkgName <> "\" -> \"" <> dropPurescript dep <> "\"")
+    fmap (\dep -> mkEdge (dropPurescript pkgName) (dropPurescript dep))
+  where
+    mkEdge x y = "\"" <> x <> "\" -> \"" <> y <> "\""
 
 dropPurescript :: Text -> Text
 dropPurescript x = fromMaybe ("JS:" <> x) $ Text.stripPrefix "purescript-" x
@@ -37,3 +41,9 @@ dropPurescript x = fromMaybe ("JS:" <> x) $ Text.stripPrefix "purescript-" x
 chromeCaps :: WDConfig
 chromeCaps =
     useBrowser chrome defaultConfig
+
+getAllPackageLinks :: WD [Text]
+getAllPackageLinks = do
+    openPage "http://localhost:3000/"
+    pkgDocLinks <- findElems (ByCSS ".multi-col__col:not(:nth-child(1)) a")
+    catMaybes <$> mapM (`attr` "href") pkgDocLinks
