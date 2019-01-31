1d9a4a6fba4ccd4cecdfdb77c25f07c6b20d93e6
Jan Hrcek <jhrcek@redhat.com>
2019-01-31 07:14:22 +0100

    Make transitive reduction optional

--- src/GraphViz.hs
+++ src/GraphViz.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE PartialTypeSignatures #-}
-module GraphViz where
+module GraphViz (showGraph, TransitiveConfig(..)) where
 
 import           Data.Graph.Inductive.Graph        (Node, subgraph)
 import           Data.Graph.Inductive.Query.DFS    (dfs)
-import           Data.GraphViz                     (GlobalAttributes (GraphAttrs),
+import           Data.GraphViz                     (DotGraph, GlobalAttributes (GraphAttrs),
                                                     GraphvizCanvas (Xlib),
                                                     GraphvizCommand (Dot),
                                                     GraphvizParams, fmtNode,
@@ -17,14 +18,22 @@ import           Data.Text.Internal                (Text)
 import           Data.Text.Lazy                    (fromStrict)
 import           TheGraph                          (theGraph)
 
-showGraph :: Node -> IO ()
-showGraph packageId = runGraphvizCanvas Dot g Xlib
+showGraph :: TransitiveConfig -> Node -> IO ()
+showGraph tc packageId = runGraphvizCanvas Dot g Xlib
   where
     depsOf nodeId = subgraph (dfs [nodeId] theGraph) theGraph
-    g = transitiveReduction $ graphToDot params $ depsOf  packageId
+    g = applyTransitiveConfig tc $ graphToDot params $ depsOf  packageId
+
 
 params :: GraphvizParams n Text el () Text
 params = nonClusteredParams
     { fmtNode = \(_nodeId, nodeLabel) -> [Label (StrLabel (fromStrict nodeLabel))]
     , globalAttributes = [GraphAttrs [RankDir FromLeft] ]
     }
+
+data TransitiveConfig = ShowTransitive | HideTransitive
+
+applyTransitiveConfig :: TransitiveConfig -> DotGraph Node -> DotGraph Node
+applyTransitiveConfig = \case
+    ShowTransitive -> id
+    HideTransitive -> transitiveReduction
